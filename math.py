335e36cf7e9800549b83729d6472dca417e38b98
ameygaonkar <ameygaonkar99@gmail.com>
2021-08-28 17:43:11 +0530

    removed function header

--- math.py
+++ math.py
@@ -1,15 +1,11 @@
-# Add implementation
 def add(x,y):
 	pass
 
-# subtract implementation
 def subtract(x,y):
 	pass
 
-# multiply implementation
 def multiply(x,y):
 	pass
 
-# divide implementation
 def divide(x,y):
 	pass
